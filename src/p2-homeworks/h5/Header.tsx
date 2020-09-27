c4ed14a2060833c3948756403f096e4ec67857b6
rdnk84 <rdnk_e@mail.ru>
2020-09-27 10:38:19 +0300

    added show/hidden option for Header menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import {NavLink} from "react-router-dom";
 import s from "./pages/pages.module.css"
 
 
 function Header() {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+    const onPanelClick =() =>{
+        setMenuIsOpen(!menuIsOpen)
+    }
     return (
         <div className={s.headerWrapper}>
-            <div className={s.item}><NavLink to={"/pre-junior"} activeClassName={s.active}>PreJunior</NavLink></div>
-            <div className={s.item}><NavLink to={"/junior"} activeClassName={s.active}>Junior</NavLink></div>
-            <div className={s.item}><NavLink to={"/juniorPlus"} activeClassName={s.active}>Junior+</NavLink></div>
-            <div className={s.item}><NavLink to={"/blabla"} activeClassName={s.active}>nothing to see</NavLink></div>
-            {/*add NavLinks*/}
+
+            <div className={menuIsOpen ? s.itemsWrapperShow: s.itemsWrapperHidden}>
+                <div className={s.item}><NavLink to={"/pre-junior"} activeClassName={s.active}>PreJunior</NavLink></div>
+                <div className={s.item}><NavLink to={"/junior"} activeClassName={s.active}>Junior</NavLink></div>
+                <div className={s.item}><NavLink to={"/juniorPlus"} activeClassName={s.active}>Junior+</NavLink></div>
+                <div className={s.item}><NavLink to={"/blabla"} activeClassName={s.active}>nothing to see</NavLink></div>
+                {/*add NavLinks*/}
+            </div>
+            <div onClick={onPanelClick} className={s.panelToClick}> <a href=""></a> </div>
         </div>
+
     );
 }
 
