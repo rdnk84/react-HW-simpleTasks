96b9dd05020bf68331674a674c365dee315c6acd
rdnk84 <rdnk_e@mail.ru>
2020-09-27 10:59:04 +0300

    Updates

---
+++ precache-manifest.ca9ba6040965b2047b599d457ecf41f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5523207d589d757ff5e13009a91c64",
+    "url": "/react-HW-simpleTasks/index.html"
+  },
+  {
+    "revision": "3ee702cb744712ef877c",
+    "url": "/react-HW-simpleTasks/static/css/main.6dd9ed34.chunk.css"
+  },
+  {
+    "revision": "5d097c25082e873033c9",
+    "url": "/react-HW-simpleTasks/static/js/2.accdb019.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-HW-simpleTasks/static/js/2.accdb019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee702cb744712ef877c",
+    "url": "/react-HW-simpleTasks/static/js/main.12744f0a.chunk.js"
+  },
+  {
+    "revision": "ec1fd3841f05ea214816",
+    "url": "/react-HW-simpleTasks/static/js/runtime-main.c4f6e28e.js"
+  }
+]);
